41474f5c8c5bdeccdda5e492f48878495ebd9e02
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2022-03-19 17:26:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const image1 = document.getElementById("image1");
+
+image1.addEventListener("click", (event) => {
+  event.target.style.background = "red";
+  event.target.style.color = "white";
+});
